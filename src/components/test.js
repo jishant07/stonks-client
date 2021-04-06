465083465565d9bd3ed15642d6805460bd3bdf1d
jishant07 <jishanta@gmail.com>
2021-04-06 13:05:28 +0530

    Working Test Component

--- src/components/test.js
+++ src/components/test.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Loader from 'react-loader'
 
 class Test extends Component {
-    state = { data:[] }
+    state = { data:[] ,loaded:false}
     async componentDidMount() {
         axios.post("https://finalyearbackend.herokuapp.com/get_file",{
-            "file_location":"https://uploads-jottit.s3.ap-south-1.amazonaws.com/ASIANPAINT.BO.csv"
+            "file_location":"https://uploads-jottit.s3.ap-south-1.amazonaws.com/aapl.csv"
         }).then(async (res) => {
             await this.setState(res.data)
+            console.log(this.state.data)
+            this.setState({loaded:true})
         })
     }
     render() {
+        var i = 0;
         var mappedData = this.state.data.map(row => {
-            return <p>{row.date}</p>
+            return (
+                <div key={i++}>
+                    <p>{row.Date}</p> <p>{row.Open}</p>
+                </div>
+            )
         })
-        return ( <div>
+        return ( 
+            <Loader loaded={this.state.loaded}>
+        <div>
             {mappedData}
-        </div> );
+        </div></Loader> );
     }
 }
  
